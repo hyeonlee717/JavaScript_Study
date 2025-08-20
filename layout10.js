394033b4a567e50990ce027a55a6b3d3770f885f
Hyeon Lee <103764100+hyeonlee717@users.noreply.github.com>
2025-08-20 15:46:38 +0900

    array object data-type

--- layout10.js
+++ layout10.js
@@ -7,14 +7,14 @@
 // }) 아직 안배운거
 
 // for 반복문 : for (){}
-for (let i = 0; i < 3; i++) { // 콤마가 아니라 ; 써야함
-    $('.tab-button').eq(i).on('click', function () {
-        $('.tab-button').removeClass('orange');
-        $('.tab-button').eq(i).addClass('orange');
-        $('.tab-content').removeClass('show');
-        $('.tab-content').eq(i).addClass('show');
-    })
-}
+// for (let i = 0; i < 3; i++) { // 콤마가 아니라 ; 써야함
+//     $('.tab-button').eq(i).on('click', function () {
+//         $('.tab-button').removeClass('orange');
+//         $('.tab-button').eq(i).addClass('orange');
+//         $('.tab-content').removeClass('show');
+//         $('.tab-content').eq(i).addClass('show');
+//     })
+// }
 
 // var이 아니라 let을 사용하는 이유는
 // var는 이놈을 감싸는 function을 범위로하는데
@@ -34,3 +34,35 @@ for (let i = 0; i < 3; i++) { // 콤마가 아니라 ; 써야함
 // 쉽게 말해서 if문 3개가 있는데
 // var을 쓰면 그 어떤 작업이든지 i = 3만을 가지고 실행하려고 접근하는 것이랑 같고
 // let을 쓰면 각 버튼에 해당하는 i값을 가지고 실행하려고 접근하는 것임
+
+// for (let i = 0; i < 3; i++) { // 콤마가 아니라 ; 써야함
+//     $('.tab-button').eq(i).on('click', function () {
+//         탭열기(i);
+//     })
+// }
+
+// 이벤트버블링 때문에 어차피 tab-button을 클릭해도 list도 눌리니까
+// $('.list').click(function (e) {
+//     if (e.target == document.querySelectorAll('.tab-button')[0]) {
+//         탭열기(0);
+//     }
+//     if (e.target == document.querySelectorAll('.tab-button')[1]) {
+//         탭열기(1);
+//     }
+//     if (e.target == document.querySelectorAll('.tab-button')[2]) {
+//         탭열기(2);
+//     }
+// })
+
+// 해당 html요소에 data-이름=''을 몰래 숨겨놓는 방법(반복문 사용x)
+$('.list').click(function (e) {
+    let id = Number(e.target.dataset.id); // 문자열을 숫자(정수)로 변환
+    탭열기(id);
+})
+
+function 탭열기(숫자) { // 함수로 축약해서 재사용 하고자 할 때는 파라미터 추가해야함
+    $('.tab-button').removeClass('orange');
+    $('.tab-button').eq(숫자).addClass('orange');
+    $('.tab-content').removeClass('show');
+    $('.tab-content').eq(숫자).addClass('show');
+}
